da3de211f4f3afe808c0f3830a5b39aebbba1589
DoanhTran03 <doanhtran2003@gmail.com>
2024-01-16 18:01:41 -0500

    Build message obejct with destination and body information

--- index.ps1
+++ index.ps1
@@ -1,3 +1,15 @@
+#Import MailKit and MimeKit package from : https://www.nuget.org/api/v2/
 Add-Type -Path "D:\Package\MailKit\lib\netstandard2.0\MailKit.dll"
 Add-Type -Path "D:\Package\MimeKit\lib\netstandard2.0\MimeKit.dll"
 
+#Create support object based on Mailkit document : http://www.mimekit.net/docs/html/Introduction.htm
+$Message = New-Object MimeKit.MimeMessage
+$MailboxAddress = New-Object MimeKit.MailboxAddress("Ngoc Doanh", "N/A")
+$Body_Text = New-Object MimeKit.TextPart("Hello this is test content")
+
+#Create message object from Mailkit namespace
+$Message.From.Add($MailboxAddress::Parse("gofortest79@outlook.com"))
+$Message.To.Add($MailboxAddress::Parse("gofortest79@outlook.com"))
+$Message.Subject = "Test Subject"
+$Message.Body = $Body_Text
+$Message
