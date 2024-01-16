9479a63e323dcba439a7b66bf7fa2142a4d74ee5
DoanhTran03 <doanhtran2003@gmail.com>
2024-01-16 18:11:14 -0500

    Create SMTP object and add information and create connection

--- index.ps1
+++ index.ps1
@@ -2,7 +2,7 @@
 Add-Type -Path "D:\Package\MailKit\lib\netstandard2.0\MailKit.dll"
 Add-Type -Path "D:\Package\MimeKit\lib\netstandard2.0\MimeKit.dll"
 
-#Create support object based on Mailkit document : http://www.mimekit.net/docs/html/Introduction.htm
+#Create support object for message based on Mailkit document : http://www.mimekit.net/docs/html/Introduction.htm
 $Message = New-Object MimeKit.MimeMessage
 $MailboxAddress = New-Object MimeKit.MailboxAddress("Ngoc Doanh", "N/A")
 $Body_Text = New-Object MimeKit.TextPart("Hello this is test content")
@@ -12,4 +12,15 @@ $Message.From.Add($MailboxAddress::Parse("gofortest79@outlook.com"))
 $Message.To.Add($MailboxAddress::Parse("gofortest79@outlook.com"))
 $Message.Subject = "Test Subject"
 $Message.Body = $Body_Text
-$Message
+
+#Create support object from SMTP
+$Option = New-Object MailKit.Security.SecureSocketOptions
+$Account = Import-Clixml -Path "C:\Scripts\Creds\outlook.xml"
+
+#Create SMTP information and connection
+$SMTP = New-Object MailKit.Net.Smtp.SmtpClient
+$SMTP.Connect("smtp-mail.outlook.com", 587, $Option::StartTls)
+$SMTP.Authenticate($Account) 
+$SMTP.Send($Message)
+$SMTP.Disconnect($true)
+
