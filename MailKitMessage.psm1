25585ff5254a3bf85a4ea9d470938708f5c9d399
DoanhTran03 <doanhtran2003@gmail.com>
2024-01-16 20:03:38 -0500

    Fix TextBody parsing issue and set Port and Credential params as mandatory

--- MailKitMessage.psm1
+++ MailKitMessage.psm1
@@ -2,33 +2,40 @@ Add-Type -Path "D:\Package\MailKit\lib\netstandard2.0\MailKit.dll"
 Add-Type -Path "D:\Package\MimeKit\lib\netstandard2.0\MimeKit.dll"
 
 function Send-MailKitMessage {
-    param(
+    [CmdletBinding()]
+    Param(
         [Parameter(Mandatory)][String] $From,
         [Parameter(Mandatory)][String] $To,
-        [Parameter()]
-        [Parameter()] $CC,
+        [Parameter()]$CC,
         [Parameter()][String] $Subject = "",
         [Parameter()][String] $Body = "",
         [Parameter(Mandatory)][string] $SMTPServer,
-        [Parameter()][Int16] $Port, 
-        [Parameter()]$Credential
+        [Parameter(Mandatory)][Int16] $Port, 
+        [Parameter(Mandatory)]$Credential
     )
     
     $Message = New-Object MimeKit.MimeMessage
     $SMTP = New-Object MailKit.Net.Smtp.SmtpClient
 
+    #From and To
     $MailboxAddress = New-Object MimeKit.MailboxAddress("Ngoc Doanh", "N/A")
     $Message.From.Add($MailboxAddress::Parse($From))
     $Message.To.Add($MailboxAddress::Parse($To))
     
+    #CC
     if ($CC) {
         foreach ($address in $CC) {
             $Message.Cc.Add($address)
-        }
+        }       
     }
 
+    #Subject
     $Message.Subject = $Subject
-    $Message.Body = $Body
+
+    #Body
+    $Body_Text = New-Object MimeKit.TextPart
+    $Body_Text.Text = $Body
+    $Message.Body = $Body_Text
 
     #Create SMTP Object and add make network connection
     $Option = New-Object MailKit.Security.SecureSocketOptions
---
+++ test.ps1
@@ -0,0 +1,3 @@
+Import-Module "D:\Project\Powershell\Send-MailKitMessage\MailKitMessage.psm1"
+$Credential = Import-Clixml "C:\Scripts\Creds\outlook.xml"
+Send-MailKitMessage -From "gofortest79@outlook.com" -To "gofortest79@outlook.com" -Subject "Hello" -Body "This is just a test" -SMTPServer "smtp-mail.outlook.com" -Port 587 -Credential $Credential
