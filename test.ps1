2c3eb442c746156d9907723cdd2708008a8e689a
DoanhTran03 <doanhtran2003@gmail.com>
2024-01-17 21:43:43 -0500

    Add Readme.md file

--- test.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module "D:\Project\Powershell\Send-MailKitMessage\MailKitMessage.psm1"
-$Credential = Import-Clixml "C:\Scripts\Creds\outlook.xml"
-$body = "<h1>Hello</h1>"
-Send-MailKitMessage -From "gofortest79@outlook.com" -To "gofortest79@outlook.com" -Subject "Hello" -Body $body -SMTPServer "smtp-mail.outlook.com" -Port 587 -Credential $Credential -Attachment "C:\test.png" -BodyAsHTML
